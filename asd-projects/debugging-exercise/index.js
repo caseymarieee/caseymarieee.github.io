170d253f024056aa1b0e526a919b5a455bdd5289
caseymarieee <casey.washington@inspire35.org>
2026-02-23 16:14:43 +0000

    asd to do 3

--- asd-projects/debugging-exercise/index.js
+++ asd-projects/debugging-exercise/index.js
@@ -45,7 +45,7 @@ function makeGhost(id) {
   var ghost = {};
 
   // this creates some useful variables that are not directly placed in the object
-  var maxX = boardWidth - ghostRadius * 2;
+  var maxx = boardWidth - ghostRadius * 2;
   var maxY = boardHeight - ghostRadius * 2;
 
   // this gives the ghost object all of the data that it needs to store
@@ -80,11 +80,11 @@ function decideSpeed() {
 
 // this generates an id for a ghost given the ghost's number
 function getId(number) {
-  return "ghost" + number; ()
+  return "ghost" + number; 
 }
 
 // this adds a ghost into the HTML
-funtion addNewGhostElement(ghost, id) {
+function addNewGhostElement(ghost, id) {
   // this creates the HTML for a new ghost element
   var $ghost = $("<img>")
     .attr("id", id)
@@ -102,7 +102,7 @@ funtion addNewGhostElement(ghost, id) {
 //////////////////
 
 // this should move all of the ghosts
-function update) {
+function update() {
   // loop over the ghosts array. We use the maxGhosts variable instead of ghosts.length
   // to make seeing issues in the debugger slightly easier (in practice, you should use
   // ghosts.length, but do NOT change it here)
@@ -136,12 +136,12 @@ function moveGhost(ghost) {
 // this bounces ghosts if they hit a wall
 function bounceGhost(ghost) {
   // this bounces off the left wall
-  if (ghost.x < 0{
+   ghost.x < 0; {
     ghost.x -= ghost.speedX;
     ghost.speedX *= -1;
-  }
+  } 
   // this bounces off the right wall
-  else if (ghost.x > boardWidth) {
+    if (ghost.x > boardWidth) {
     ghost.x -= ghost.speedX;
     ghost.speedX *= -1;
   }
